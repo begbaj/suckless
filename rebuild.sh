93e429f7f37babea4c9232117e9e05ffc941c69e
Began <beganbajrami@gmail.com>
2022-10-16 23:05:42 +0200

    refactor

--- rebuild.sh
+++ rebuild.sh
@@ -1,14 +1,25 @@
 
 #!/bin/sh
-make clean -C ./st/st/
-make clean -C ./dmenu/dmenu/
-make clean -C ./dwm/dwm/
+make clean -C ./st/src/
+make clean -C ./dmenu/src/
+make clean -C ./dwm/src/
+make clean -C ./slock/src/
+make clean -C ./surf/src/
+make clean -C ./quark/src/
 
 make -C ./st/st/
 make -C ./dmenu/dmenu/
 make -C ./dwm/dwm/
+make -C ./slock/src/
+make -C ./surf/src/
+make -C ./quark/src/
 
 sudo make install -C ./st/st/
 sudo make install -C ./dmenu/dmenu/
 sudo make install -C ./dwm/dwm/
+sudo make install -C ./slock/src/
+sudo make install -C ./surf/src/
+sudo make install -C ./quark/src/
+
+killall rsblocks
 killall dwm
